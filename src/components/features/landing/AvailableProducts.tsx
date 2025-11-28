1463521090789d4115a99da443c3472d88b2737e
Rezki Triandy <rezki.hifella@gmail.com>
2025-11-28 10:17:32 +0700

    update grid gap

--- src/components/features/landing/AvailableProducts.tsx
+++ src/components/features/landing/AvailableProducts.tsx
@@ -5,7 +5,7 @@ const AvailableProducts = () => {
   return (
     <div className="space-y-8">
       <h1 className="text-2xl font-bold text-primary-black">Produk Tersedia</h1>
-      <div className="grid grid-cols-5">
+      <div className="grid grid-cols-5 gap-4">
         {Array.from({ length: 8 }).map((_, index) => (
           <ProductCard key={`availproduct-${index}`} />
         ))}
